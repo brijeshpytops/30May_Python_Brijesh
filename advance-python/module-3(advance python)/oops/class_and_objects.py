fd3337e7d3a0cfae6b28a9460ae1dae6c8d0ab8a
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-07-08 09:34:23 +0530

    class object constructor and destructor

---
+++ advance-python/module-3(advance python)/oops/class_and_objects.py
@@ -0,0 +1,35 @@
+# """
+# syntax :
+# class class_name:
+#     body of class
+
+# obj_name = class_name() // object
+    
+# """
+# class Animal:
+#     # data member (attribute or property)
+#     name = "dog"
+
+#     # member function (function or method)
+#     def voice(yoyo):
+#         print("bark")
+
+# d1 = Animal() # object
+
+# print(d1.name) # Access attribute
+# d1.voice() # access method
+
+# class person:
+#     # constructor/destructor
+#     def __init__(self, name, age):
+#         self.n = name
+#         self.a = age
+
+#     def display(self):
+#         print(f"Name : {self.n}")
+#         print(f"Age : {self.a}")
+
+# rohit = person("Rohit Panchal", 35)
+# rohit.display()
+# print(rohit.a)
+# print(rohit.n)
